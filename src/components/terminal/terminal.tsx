ad08b2d3cddf0c640e96ca52224f91ca60ea0556
a Keech <andy.keech@live.ca>
2018-09-13 13:00:40 -0400

    Added styling and commands

--- src/components/terminal/terminal.tsx
+++ src/components/terminal/terminal.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import { Container, Segment, Header, Input } from "semantic-ui-react";
 import "./terminal.css";
 
-class Terminal extends React.Component<{}, { enteredCommands: string[]; currentInput: "" }> {
-  constructor(props: {}) {
+class Terminal extends React.Component<any, { enteredCommands: string[]; currentInput: "" }> {
+    private inputElement: any;
+  constructor(props: any) {
     super(props);
     this.state = { enteredCommands: [], currentInput: "" };
     this.handleChange = this.handleChange.bind(this);
+    this.inputElement = React.createRef();
+    this.focus = this.focus.bind(this);
   }
 
   public render() {
@@ -15,17 +18,22 @@ class Terminal extends React.Component<{}, { enteredCommands: string[]; currentI
         <Header attached="top" className="terminal-header">
           AK Term
         </Header>
-        <Segment raised={true} attached={true} className="terminal-body">
+        <Segment onClick={this.focus} raised={true} attached={true} className="terminal-body">
           {this.state.enteredCommands.map((command, index) => (
-            <p className="command-output" key={index}>
-              > {command}
-            </p>
+            <div className="command-output" key={index}>
+              {this.evaluateInput(command)}
+            </div>
           ))}
-           <Input icon='angle right' iconPosition='left' fluid={true} value={this.state.currentInput} onChange={this.handleChange} onKeyPress={this.handleKeyPress} className="terminal-input" transparent={true} placeholder="input command here" />
+          <Input autoFocus={true} ref={this.inputElement} icon="angle right" iconPosition="left" fluid={true} value={this.state.currentInput} onChange={this.handleChange} onKeyPress={this.handleKeyPress} className="terminal-input" transparent={true} placeholder="input command here" />
         </Segment>
       </Container>
     );
   }
+
+  private focus() {
+    this.inputElement.current.focus();
+  }
+
 
   private handleChange(event: any) {
     this.setState({ currentInput: event.target.value });
@@ -38,6 +46,33 @@ class Terminal extends React.Component<{}, { enteredCommands: string[]; currentI
       this.setState({ currentInput: "" });
     }
   };
+
+  private evaluateInput(input: string): any {
+    switch (input.toLowerCase()) {
+      case "contact":
+        return (
+          <Container>
+            > {input} <br />
+            <span className="bold-green">Email:</span> Andy@akeech.com <br />
+            <span className="bold-green">Phone:</span> (226) 236-6662
+          </Container>
+        );
+      case "help":
+        return (
+          <Container>
+            > {input} <br />
+            <span className="blue">Commands Are:</span> contact, help <br />
+          </Container>
+        );
+      default:
+        return (
+          <Container>
+            > {input} <br />
+            Unknown command "{input}", type "help" for a list of commands
+          </Container>
+        );
+    }
+  }
 }
 
 export default Terminal;
