8e8535abfd4c02390c0f9d5b70a603a0dc98fd03
a Keech <andy.keech@live.ca>
2018-09-20 12:26:06 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const os = require("os");
 const app = express();
 
 var port = process.env.PORT || 3000;
-app.use(express.static("build"));
+app.use(express.static("./build"));
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log("Listening on port " + port));
